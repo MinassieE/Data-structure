c0dedb8586263bfbd3512c90c5a5d430a930cbcf
MinassieE <menaephrem17@gmail.com>
2023-04-20 12:05:46 +0300

    add the push method to the stack implementation

--- in-javascript/stack.js
+++ in-javascript/stack.js
@@ -11,4 +11,16 @@ class Stack {
         this.top = newNode;
         this.length = 1;
     }
+
+    push(value){
+        const newNode = new Node(value);
+
+        newNode.next = this.top;
+        this.top = newNode;
+        this.length++;
+        return this;
+    }
 }
+
+let myStack = new Stack(5);
+console.log(myStack.push(6));
