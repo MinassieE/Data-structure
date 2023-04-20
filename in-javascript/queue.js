668bbdfcd636d8565a131e7cddc1f0001b33da7a
MinassieE <menaephrem17@gmail.com>
2023-04-20 12:25:35 +0300

    add the enqueue method

--- in-javascript/queue.js
+++ in-javascript/queue.js
@@ -13,5 +13,21 @@ class Queue {
         this.length = 1;
     }
 
-    
+    enqueue(value){
+        const newNode = new Node(value);
+        if(this.length === 0){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            newNode.next = this.last;
+            this.last = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
 }
+
+let myQueue = new Queue(5);
+console.log(myQueue.enqueue(6));
